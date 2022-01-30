bfd2a2fd67bd8e51f825b0d282d652dd3f92a381
Sifat07 <sifat140693@gmail.com>
2022-01-31 03:01:37 +0600

    feat: Intro page

---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import Sidebar from "../components/Intro";
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
