b4709b3f16f3ab2f8f03825589e9c3976250c930
Sifat07 <sifat140693@gmail.com>
2022-01-31 11:00:04 +0600

    feat: use Next Image instead of Background image in scss for faster background image loading

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,21 @@ import React from "react";
 import Intro from "../components/Intro";
 import Nav from "../components/Nav";
 import styles from "./index.module.scss";
+import Image from "next/image";
+import bg from "../components/assets/images/IMG_1049.png";
 
 const index = () => {
   return (
     <div>
       <div className={styles.section1}>
+        <Image
+          alt="Mountains"
+          src={bg}
+          layout="fill"
+          objectFit="cover"
+          quality={100}
+          placeholder="blur"
+        />
         <Nav />
         <Intro />
       </div>
